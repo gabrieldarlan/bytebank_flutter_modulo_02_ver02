49709905bf6931be9f9a26fcf878434c2f139df2
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-11 21:16:34 -0300

    aula 04 - 03 carregando a lista de contatos

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:bytebank_dashboard/database/app_database.dart';
+import 'package:bytebank_dashboard/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+import 'models/contact.dart';
+
+void main() {
+  runApp(BytebankApp());
+  save(Contact(3, 'maria', 1000));
+  // .then((id) {
+  // findAll().then((contacts) => debugPrint(contacts.toString()));
+  // });
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Dashboard(),
+    );
+  }
+}
